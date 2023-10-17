01d1eb13a07081f1db701f82a422f5680cb15140
minzlzl <133857306+minzlzl@users.noreply.github.com>
2023-10-17 17:16:00 +0900

    Delete app directory

--- app/api/route.js
+++
@@ -1,5 +0,0 @@
-import { qe } from './db';
-export async function GET() {
-    let data = await qe('SELECT * from dm_member');
-    return Response.json(data);
-}
--- app/pages/rank/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function page() {
-  return (
-    <div>
-      랭킹이요~
-    </div>
-  )
-}
